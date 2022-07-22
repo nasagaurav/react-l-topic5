d11d1dab2569a946ad50b7a0f5cadad23ba5f0de
nasagaurav <nasa.gauravnasa@gmail.com>
2022-07-22 07:38:10 +0530

    created example2 started

--- src/Example2.js
+++ src/Example2.js
@@ -1,17 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Example2() {
+  const [x, setx] = useState(0);
+  const [y, sety] = useState(0);
+  const [z, setz] = useState(0);
+
+  const p1 = () => setx(x + 1);
+  const p2 = () => sety(y + 1);
+  const p3 = () => setz(z + 1);
   return (
     <div>
       <Title />
       <Text value={x} />
       <Text value={y} />
       <Text value={z} />
-      <Button>+x</Button>
-      <Button>+y</Button>
-      <Button>+z</Button>
+      <Button onClick={p1}>+x</Button>
+      <Button onClick={p2}>+y</Button>
+      <Button onClick={p3}>+z</Button>
     </div>
   );
 }
-
 export default Example2;
+
+function Title() {
+  console.log('title comp');
+  return <h1>title</h1>;
+}
+function Button() {
+  console.log('button comp');
+  return <button>button</button>;
+}
+function Text(props) {
+  console.log('text comp');
+  return <p>{props.value}</p>;
+}
