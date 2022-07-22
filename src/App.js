933062c29e509e56ac78b46d9c92e1dda4ed7abc
nasagaurav <nasa.gauravnasa@gmail.com>
2022-07-22 07:16:38 +0530

    added Example1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "./style.css";
-
+import Example1 from './Example1'
 export default function App() {
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
+      <Example1/>
     </div>
   );
 }
---
+++ src/Example1.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Example1()
+{
+  return <div>
+    <h1>Example1</h1>
+  </div> 
+}
+
+export default Example1;
