fae7812d2ad9b62974d0e22b555652fe9b9f1b60
nasagaurav <nasa.gauravnasa@gmail.com>
2022-07-22 07:33:59 +0530

    example1 code restored

--- src/App.js
+++ src/App.js
@@ -3,9 +3,13 @@ import './style.css';
 import Example2 from './Example2';
 import Example3 from './Example3';
 import Example4 from './Example4';
+const Example1 = lazy(() => import('./Example1'));
 export default function App() {
   return (
     <div>
+      <Suspense fallback={'loading...'}>
+        <Example1 />
+      </Suspense>
       <Example2 />
       <Example3 />
       <Example4 />
