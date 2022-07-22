d37650d1473028227e7201c0c4652cf43981021e
nasagaurav <nasa.gauravnasa@gmail.com>
2022-07-22 07:26:23 +0530

    added lazy load and suspense

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
-import React from "react";
-import "./style.css";
-import Example1 from './Example1'
-import Example2 from './Example2'
-import Example3 from './Example3'
-import Example4 from './Example4'
+import React, { lazy, Suspense } from 'react';
+import './style.css';
+import Example2 from './Example2';
+import Example3 from './Example3';
+import Example4 from './Example4';
+// import Example1 from './Example1'
+const Example1 = lazy(() => import('./Example1'));
 export default function App() {
   return (
     <div>
-      <Example1/>
-      <Example2/>
-      <Example3/>
-      <Example4/>
+      <Suspense fallback={'loading...'}>
+        <Example1 />
+      </Suspense>
+      <Example2 />
+      <Example3 />
+      <Example4 />
     </div>
   );
 }
--- src/Example1.js
+++ src/Example1.js
@@ -1,10 +1,21 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 
-function Example1()
-{
-  return <div>
-    <h1>Example1</h1>
-  </div> 
+function Example1() {
+  const [a, seta] = useState([]);
+
+  const boot = () => {
+    const url = 'https://jsonplaceholder.typicode.com/photos';
+    fetch(url)
+      .then((d) => d.json())
+      .then((d) => seta(d));
+  };
+  useEffect(boot, []);
+  return (
+    <div>
+      <h1>Example1</h1>
+      {/* {a.map(x=><img  width="10" src={x.url} />)} */}
+    </div>
+  );
 }
 
 export default Example1;
